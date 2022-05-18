b5e41601727b8e91932468d5c1ea44eea7a50300
Sultnkh <47182157+Sultnkh@users.noreply.github.com>
2022-05-18 13:13:07 +0300

    Get on that ride challenge

--- Rushud_Sultan_JS_CreateVar.js
+++ Rushud_Sultan_JS_CreateVar.js
@@ -1,4 +1,11 @@
 var ReqHeight = 42;
 var ReqAge = 10;
-var Height = 0; //This variable needs to be set to N>=42
+var Height = 52; //This variable needs to be set to N>=42
 var Age = 0; //This variable needs to be set to N>=10
+
+if(height>ReqHeight){
+    console.log("Get on that ride, Kiddo ;)")
+}
+else{
+    console.log("Sorry Kiddo, Maybe next year!")
+}
